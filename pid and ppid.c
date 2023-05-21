55b56542c666cfeb25da4288ba355f701b92e0f1
mibonacci <mohammadramdaoui@gmail.com>
2023-05-21 21:27:31 +0100

    argumets

--- pid and ppid.c
+++ pid and ppid.c
@@ -2,18 +2,28 @@
 #include <stdio.h>
 #include <sys/types.h>
 
+
+
 /**
- * pid - PID main function
+ * printing the process id and the parent pid
+ * printing all the arguments without using ac
  * 
  * Return - always 0
 */
 
-int main ()
+int main (int ac, char **av)
 {
     pid_t mypid, myppid;
-
+    (void)ac;
+    int i = 0;
     mypid = getpid();
     myppid = getppid();
+
+    while (av[i] != NULL, av[i])
+    {
+        printf("%s\n", av[i]);
+        i++;
+    }
     printf("My pid: %d\nMy ppid: %d\n", myppid, mypid);
     return 0;
 }
