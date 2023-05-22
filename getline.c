d4bb1933bf366fdfd9525d3918279504bbe2552e
mibonacci <mohammadramdaoui@gmail.com>
2023-05-23 00:37:35 +0100

    getline

--- getline.c
+++ getline.c
@@ -5,7 +5,19 @@
  * at the start of each line 
  */
 
-int main(void)
+int main (int ac, char **av)
 {
+    char *buffer = NULL;
+    size_t size_buff = 0;
+    int n_char = 0;
     
+    write(1, "$ ", 2);
+    while (getline(&buffer, &size_buff, stdin) != -1)
+    {
+        n_char = strlen(buffer);
+        write(1, buffer, n_char);
+        write(1, "\n", 1);
+    }
+        free(buffer);
+        return 0;
 }
