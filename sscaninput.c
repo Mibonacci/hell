ea8999d2934b1abc49394dfec8efad580b23cb5e
Ancient-mahou <naeturx@gmail.com>
2023-05-24 22:39:35 +0100

    Betty update

--- sscaninput.c
+++ sscaninput.c
@@ -1,10 +1,10 @@
 #include "headers.h"
 
 /**
- * scaninput - this fucntion scans the input input 
- * from the user, handling also the $ prompt at the start of each line
- * 
-*/
+ * scaninput - this function scans the input input
+ * from the user, handling also the $ prompt at the
+ * start of each line
+ */
 
 char *scaninput(pathing *current)
 {
@@ -12,17 +12,17 @@ char *scaninput(pathing *current)
 	size_t a = 0;
 	char *input = NULL;
 
-    write(1, "$ ", 2);
+	write(1, "$ ", 2);
 	read = getline((&input, &a, stdin) != -1);
-	
+
 	if (read == 1 && input[0] == '\n')
 	{
 		free(input);
 		return (NULL);
 	}
-    
-    /*to escape the EOF loop*/
-    if (read == EOF)
+
+	/*to escape the EOF loop*/
+	if (read == EOF)
 	{
 		write(STDOUT_FILENO, "\n", 1);
 		free_list(current);
